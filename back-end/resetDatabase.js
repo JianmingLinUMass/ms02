1117d39aa3820a8a2b3983a7d16e35cd5dc7c806
LoickMarion <loick.marion@gmail.com>
2024-12-04 18:52:44 -0500

    Adding Exercise Questions (#47): Came up with some past tense questions. These need to be stored in the database so they can be pulled to the front end.

--- back-end/resetDatabase.js
+++ back-end/resetDatabase.js
@@ -1,8 +1,11 @@
 const Database = require('./database.js');  // Assuming your database.js file is in the same directory
 const db = new Database('./questions.db');  // Path to your SQLite database
 
-db.removeQuestionsTable()
-db.createQuestionsTable()
+function resetQuestionsTable(){
+    db.removeQuestionsTable()
+    db.createQuestionsTable()
+}
+
 
 // const db2 = new Database('./userAccounts.db');
 // db2.removeUserAccountsTable();
